50464f4956d514c0ccfcc22459f6f2928f520e6e
itzia <a29539@svalero.com>
2026-05-03 19:07:27 +0200

    listado completo alumnos con todos los campos

--- src/servlet/ListarAlumnosServlet.java
+++ src/servlet/ListarAlumnosServlet.java
@@ -24,12 +24,39 @@ public class ListarAlumnosServlet extends HttpServlet {
 
         out.println("<h1>Lista de alumnos</h1>");
 
+        out.println("<table border='1'>");
+        out.println("<tr>");
+        out.println("<th>ID</th>");
+        out.println("<th>Nombre</th>");
+        out.println("<th>Apellidos</th>");
+        out.println("<th>Email</th>");
+        out.println("<th>Rol</th>");
+        out.println("<th>Fecha nacimiento</th>");
+        out.println("<th>Nivel</th>");
+        out.println("<th>Activo</th>");
+        out.println("<th>Telefono</th>");
+        out.println("<th>Direccion</th>");
+        out.println("</tr>");
+
         try {
             while (rs.next()) {
-                out.println("<p>" + rs.getString("nombre") + "</p>");
+                out.println("<tr>");
+                out.println("<td>" + rs.getInt("id") + "</td>");
+                out.println("<td>" + rs.getString("nombre") + "</td>");
+                out.println("<td>" + rs.getString("apellidos") + "</td>");
+                out.println("<td>" + rs.getString("email") + "</td>");
+                out.println("<td>" + rs.getString("rol") + "</td>");
+                out.println("<td>" + rs.getDate("fecha_nacimiento") + "</td>");
+                out.println("<td>" + rs.getString("nivel") + "</td>");
+                out.println("<td>" + rs.getBoolean("activo") + "</td>");
+                out.println("<td>" + rs.getString("telefono") + "</td>");
+                out.println("<td>" + rs.getString("direccion") + "</td>");
+                out.println("</tr>");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        out.println("</table>");
     }
 }
