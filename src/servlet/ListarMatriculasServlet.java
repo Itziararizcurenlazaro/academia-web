6635869c95a974dd42fbe71f9bf9d89a9b57f52c
itzia <a29539@svalero.com>
2026-05-03 19:25:51 +0200

    listado matriculas con relaciones alumno-curso

---
+++ src/servlet/ListarMatriculasServlet.java
@@ -0,0 +1,52 @@
+package servlet;
+
+import dao.MatriculaDAO;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+
+@WebServlet("/matriculas")
+public class ListarMatriculasServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        MatriculaDAO dao = new MatriculaDAO();
+        ResultSet rs = dao.listarMatriculas();
+
+        out.println("<h1>Lista de matriculas</h1>");
+
+        out.println("<table border='1'>");
+        out.println("<tr>");
+        out.println("<th>ID</th>");
+        out.println("<th>Alumno</th>");
+        out.println("<th>Curso</th>");
+        out.println("<th>Estado</th>");
+        out.println("<th>Nota</th>");
+        out.println("</tr>");
+
+        try {
+            while (rs.next()) {
+                out.println("<tr>");
+                out.println("<td>" + rs.getInt("id") + "</td>");
+                out.println("<td>" + rs.getString("alumno") + "</td>");
+                out.println("<td>" + rs.getString("curso") + "</td>");
+                out.println("<td>" + rs.getString("estado") + "</td>");
+                out.println("<td>" + rs.getDouble("nota_final") + "</td>");
+                out.println("</tr>");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        out.println("</table>");
+    }
+}
